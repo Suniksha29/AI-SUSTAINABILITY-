3e66e0b897c9474ee289b21b385f9eebd0075b7d
Your Full Name <your@email.com>
2025-09-19 12:35:33 +0530

    Checkpoint for message ad6b93a7-74ab-4337-8618-5268caa9d907

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Greeting = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a new component.</p>
+    </div>
+  );
+};
+
+export default Greeting;
