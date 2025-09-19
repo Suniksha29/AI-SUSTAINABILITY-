f64d799cb51c1419488e3d5b665b1b1b4fbac7b2
Your Full Name <your@email.com>
2025-09-19 12:36:36 +0530

    Checkpoint for message 549446e9-ec50-4239-b31c-72f98a379c85

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import Header from "./Header";
+import Header from "./components/Header";
+import Greeting from "./components/Greeting";
 
 function App() {
   return (
     <div>
       <Header />
+      <Greeting />
       <p>This is my first React program with a header component.</p>
     </div>
   );
 }
 
 export default App;
-
